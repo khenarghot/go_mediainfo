f3fac8d6cf7e91873625eb19eb357503df0fac84
Nikita Egorov <n.egorov@restream.rt.ru>
2020-09-09 10:56:02 +0300

    Append stream support for get info

--- go_mediainfo.h
+++ go_mediainfo.h
@@ -6,7 +6,7 @@
 #include <locale.h>
 #include <limits.h>
 
-const wchar_t *toWchar(const char *c)
+static const wchar_t *toWchar(const char *c)
 {
     const size_t cSize = strlen(c)+1;
     wchar_t* wc = malloc(cSize * sizeof(wchar_t));
@@ -14,7 +14,7 @@ const wchar_t *toWchar(const char *c)
     return wc;
 }
 
-const char *toChar(const wchar_t *c)
+static const char *toChar(const wchar_t *c)
 {
     const size_t cSize = wcslen(c)+1;
     char* wc = malloc(cSize * sizeof(char));
@@ -22,37 +22,43 @@ const char *toChar(const wchar_t *c)
     return wc;
 }
 
-void *GoMediaInfo_New() {
+static void *GoMediaInfo_New() {
     return MediaInfo_New();
 }
 
-void GoMediaInfo_Delete(void *handle) {
+static void GoMediaInfo_Delete(void *handle) {
     MediaInfo_Delete(handle);
 }
 
-size_t GoMediaInfo_OpenFile(void *handle, char *name) {
+static size_t GoMediaInfo_OpenFile(void *handle, char *name) {
     return MediaInfo_Open(handle, toWchar(name));
 }
 
-size_t GoMediaInfo_OpenMemory(void *handle, char *bytes, size_t length) {
+static size_t GoMediaInfo_OpenMemory(void *handle, char *bytes, size_t length) {
     MediaInfo_Open_Buffer_Init(handle, length, 0);
     MediaInfo_Open_Buffer_Continue(handle, bytes, length);
 
     return MediaInfo_Open_Buffer_Finalize(handle);
 }
 
-void GoMediaInfo_Close(void *handle) {
+static void GoMediaInfo_Close(void *handle) {
     MediaInfo_Close(handle);
 }
 
-const char *GoMediaInfoGet(void *handle, char *name) {
+static const char *GoMediaInfoGet(void *handle, char *name) {
     return toChar(MediaInfo_Get(handle, MediaInfo_Stream_General, 0,  toWchar(name), MediaInfo_Info_Text, MediaInfo_Info_Name));
 }
 
-const char *GoMediaInfoOption(void *handle, char *name, char *value) {
+static const char *GoMediaInfoStreamGet(void *handle, MediaInfo_stream_C stream, char *name) {
+    return toChar(
+        MediaInfo_Get(handle, stream, 0, toWchar(name),
+                      MediaInfo_Info_Text, MediaInfo_Info_Name));
+}
+
+static const char *GoMediaInfoOption(void *handle, char *name, char *value) {
     return toChar(MediaInfo_Option(handle, toWchar(name), toWchar(value)));
 }
 
-const char *GoMediaInfoInform(void *handle) {
+static const char *GoMediaInfoInform(void *handle) {
     return toChar(MediaInfo_Inform(handle, 0));
 }
